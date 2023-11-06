7becff9bd9ab0d69ee0f8e4356918df75961f8e8
belarr0 <belarr00ss@gmail.com>
2023-11-06 20:49:08 +0200

    mod 8 HM done

---
+++ python-core-homework-08-main/trash/main_1.py
@@ -0,0 +1,38 @@
+from datetime import date, datetime, timedelta
+
+def get_birthdays_per_week(users):
+    today = date.today()
+    current_year = today.year
+
+    birthdays_per_week = {
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+    }
+
+    for user in users:
+        new_birthday_date = user["birthday"].replace(year=current_year)
+        day_of_week = new_birthday_date.weekday()
+        #print(day_of_week)
+        if 0 <= day_of_week <= 5:
+            day_name = list(birthdays_per_week.keys())[day_of_week-1]
+            birthdays_per_week[day_name].append(user["name"])
+        elif 5 < day_of_week < 7:
+            birthdays_per_week['Monday'].append(user["name"])
+
+    return birthdays_per_week
+
+
+if __name__ == "__main__":
+    users = [
+        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+        {"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()},
+        {"name": "Mark Zuckerberg", "birthday": datetime(1984, 5, 14).date()},
+    ]
+
+    result = get_birthdays_per_week(users)
+    print(result)
+    for day_name, names in result.items():
+        print(f"{day_name}: {', '.join(names)}")
---
+++ python-core-homework-08-main/trash/test.py
@@ -0,0 +1,30 @@
+from datetime import datetime, date
+
+# data = {"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()}
+# birthday = data["birthday"]
+# print(birthday)
+
+
+# data = {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()}
+# birthday = data["birthday"]
+# print(birthday)
+#
+# day_of_week = birthday.weekday()
+#
+# # Виведення назви дня тижня
+# days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# day_name = days_of_week[day_of_week]
+#
+# print(f"The day of the week for the birthday is: {day_name}")
+# print(day_of_week)
+
+# birthdays_per_week = {
+#         "Monday": [],
+#         "Tuesday": [],
+#         "Wednesday": [],
+#         "Thursday": [],
+#         "Friday": [],
+#     }
+#
+# birthdays_per_week['Monday'].append('12')
+# print(birthdays_per_week)
